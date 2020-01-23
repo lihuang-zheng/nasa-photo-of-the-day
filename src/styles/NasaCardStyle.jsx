6ea2eedf4ad404b55ec053e5bad2e6f0e8704ee2
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-23 16:34:25 -0600

    changes the code to make the website working again.

--- src/styles/NasaCardStyle.jsx
+++ src/styles/NasaCardStyle.jsx
@@ -1,58 +1,58 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const PhotoList = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 2% 0;
-`;
+// const PhotoList = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 2% 0;
+// `;
 
-const NasaPhoto = styled.img`
-  width: 400px;
-  height: 300px;
-  object-fit: scale;
-  flex-shrink: 2;
-  margin: 100px auto;
-  background: white;
-  border-radius: 5px;
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-  margin: 4% 0;
-`;
+// const NasaPhoto = styled.img`
+//   width: 400px;
+//   height: 300px;
+//   object-fit: scale;
+//   flex-shrink: 2;
+//   margin: 100px auto;
+//   background: white;
+//   border-radius: 5px;
+//   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+//     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+//     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+//     0 100px 80px rgba(0, 0, 0, 0.12);
+//   margin: 4% 0;
+// `;
 
-const PhotoTitle = styled.h1`
-  font-size: 1.7rem;
-  color: #4f5b66;
-  text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-`;
+// const PhotoTitle = styled.h1`
+//   font-size: 1.7rem;
+//   color: #4f5b66;
+//   text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+//     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+//     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+//     0 100px 80px rgba(0, 0, 0, 0.12);
+// `;
 
-const PhotoDate = styled.h3`
-  color: #4f5b66;
-  text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-`;
+// const PhotoDate = styled.h3`
+//   color: #4f5b66;
+//   text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+//     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+//     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+//     0 100px 80px rgba(0, 0, 0, 0.12);
+// `;
 
-const PhotoExplanation = styled.p`
-  padding: 5% 10% 0;
-  color: #65737e;
-  text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-`;
+// const PhotoExplanation = styled.p`
+//   padding: 5% 10% 0;
+//   color: #65737e;
+//   text-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+//     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+//     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+//     0 100px 80px rgba(0, 0, 0, 0.12);
+// `;
 
-export default {
-  PhotoExplanation,
-  PhotoDate,
-  PhotoTitle,
-  PhotoList,
-  NasaPhoto
-};
+// export default {
+//   PhotoExplanation,
+//   PhotoDate,
+//   PhotoTitle,
+//   PhotoList
+//   //   NasaPhoto
+// };
